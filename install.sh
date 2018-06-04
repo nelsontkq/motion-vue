ec64e0533f7bbc8c9231e23f8d1f17e952d22bb7
nelson <truran@outlook.com>
2018-06-03 20:20:11 -0700

    Install script now works on Ubuntu & Debian as well as solus.

--- install.sh
+++ install.sh
@@ -1,30 +1,91 @@
 #!/usr/bin/env bash
 
-declare -a requiredPackages=("golang" "nodejs")
-declare -a notInstalled=()
-for i in $(requiredPackages[@]); do
-    if ! [eopkg list-installed | grep -q "^$i"]; then
-        echo $i was not found...
-        notInstalled+=($i)
-    fi
+declare listInstalledPackages;
+declare installPackage;
+
+################################# Get distro ##################################
+. /etc/os-release
+while :
+do
+    case $NAME in
+        Solus)
+            listInstalledPackages="eopkg list-installed"
+            installPackage="eopkg install"
+            ;;
+        Debian)
+            listInstalledPackages="apt-get --installed"
+            installPackage="apt-get install"
+            ;;
+        Ubuntu)
+            listInstalledPackages="apt-get --installed"
+            installPackage="apt-get install"
+            ;;
+        *)
+            echo "$NAME is an unrecognised/unsupported distro."
+    esac
 done
-read -r -p "Would you like to install the packages? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-    installLine=$(printf "%s " "$notInstalled[@]")
-    eopkg install $installLine
-else
+# Possibly unnecessary future-proofing.
+unset NAME
+unset VERSION
+unset ID
+unset VERSION_ID
+unset PRETTY_NAME
+unset ANSI_COLOR
+unset HOME_URL
+unset SUPPORT_URL
+unset BUG_REPORT_URL
+###############################################################################
+
+################################ Check for root ###############################
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+###############################################################################
+
+################## Check required packages are installed. #####################
+declare -a requiredPackages=("golang" "nodejs")
+declare notInstalled=""
+for i in "${requiredPackages[@]}"; do
+  $listInstalledPackages | grep -lq "^$i"
+  if [ $? -ge 1 ]; then
+    notInstalled+=" $i"
+  fi
+done
+if [ ${#notInstalled} -ge 1 ]; then
+  echo Following packages not installed: $notInstalled
+  read -r -p "Would you like to install the packages? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+  then
+    $installPackage $notInstalled
+  else
     echo Required packages not installed. Aborting operation...
     exit 1
+  fi
 fi
+###############################################################################
 
-
+######################### Install and config backend ##########################
 go get "github.com/gorilla/mux"
 go get "github.com/rs/cors"
 cd src/video-host
 go build -o video-server
-
-if ! [ -d $HOME/.motion/ ]; then 
-    mkdir $HOME/.motion/
+USER_HOME=$(eval echo ~${SUDO_USER})
+if [ -d "${USER_HOME}/.motion" ]; then 
+  if ! [ -L "$PWD/motion" ]; then
+    echo symlinking ${USER_HOME}/.motion/ to $PWD/motion
+    ln -s ${USER_HOME}/.motion/ $PWD/motion
+  fi
+else
+  echo motion path not found! please enter path to motion output directory:
+  read -e -p "Motion Path: " motionPath
+  if [ -d $motionPath ]; then
+    ln -s $motionPath $PWD/motion
+  else
+    echo Invalid location. Exiting
+    exit 1
+  fi
 fi
-ln -s $HOME/.motion/ $PWD/motion
+###############################################################################
+echo Install complete.
+exit 0
